18bdcfecab7fe9b90140c2e2e6a7f1d5eb65a72c
Jonathan LEI <me@xjonathan.dev>
2023-12-30 22:26:59 +0000

    feat: scanner status

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,14 @@ use std::fmt::Display;
 
 use reqwest::Client;
 
-use serde::{Deserialize, Serialize};
+use serde::de::DeserializeOwned;
 pub use url::Url;
+
+pub mod capabilities;
+use capabilities::ScannerCapabilities;
+
+pub mod status;
+use status::ScannerStatus;
 
 #[derive(Debug)]
 pub struct Scanner {
@@ -17,160 +23,6 @@ pub enum Error {
     Xml(serde_xml_rs::Error),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct ScannerCapabilities {
-    pub version: String,
-    pub make_and_model: String,
-    pub manufacturer: String,
-    pub serial_number: String,
-    #[serde(rename = "UUID")]
-    pub uuid: String,
-    #[serde(rename = "AdminURI")]
-    pub admin_uri: String,
-    #[serde(rename = "IconURI")]
-    pub icon_uri: String,
-    pub certifications: Certifications,
-    pub platen: Platen,
-    pub compression_factor_support: CompressionFactorSupport,
-    pub supported_media_types: SupportedMediaTypes,
-    pub sharpen_support: SharpenSupport,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct Certifications {
-    pub certification: Certification,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct Certification {
-    pub name: String,
-    pub version: String,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct Platen {
-    pub platen_input_caps: PlatenInputCaps,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct PlatenInputCaps {
-    pub min_width: u32,
-    pub max_width: u32,
-    pub min_height: u32,
-    pub max_height: u32,
-    pub max_scan_regions: u32,
-    pub setting_profiles: SettingProfiles,
-    pub supported_intents: SupportedIntents,
-    pub max_optical_x_resolution: u32,
-    pub max_optical_y_resolution: u32,
-    pub risky_left_margin: u32,
-    pub risky_right_margin: u32,
-    pub risky_top_margin: u32,
-    pub risky_bottom_margin: u32,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct SettingProfiles {
-    pub setting_profile: SettingProfile,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct SettingProfile {
-    pub color_modes: ColorModes,
-    pub content_types: ContentTypes,
-    pub document_formats: DocumentFormats,
-    pub supported_resolutions: SupportedResolutions,
-    pub color_spaces: ColorSpaces,
-    pub ccd_channels: CcdChannels,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct ColorModes {
-    pub color_mode: Vec<String>,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct ContentTypes {
-    pub content_type: Vec<String>,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct DocumentFormats {
-    pub document_format: Vec<String>,
-    pub document_format_ext: Vec<String>,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct SupportedResolutions {
-    pub discrete_resolutions: DiscreteResolutions,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct DiscreteResolutions {
-    pub discrete_resolution: Vec<DiscreteResolution>,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct DiscreteResolution {
-    pub x_resolution: u32,
-    pub y_resolution: u32,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct ColorSpaces {
-    pub color_space: String,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct CcdChannels {
-    pub ccd_channel: String,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct SupportedIntents {
-    pub intent: Vec<String>,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct CompressionFactorSupport {
-    pub min: u32,
-    pub max: u32,
-    pub normal: u32,
-    pub step: u32,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct SupportedMediaTypes {
-    pub media_type: String,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct SharpenSupport {
-    pub min: u32,
-    pub max: u32,
-    pub normal: u32,
-    pub step: u32,
-}
-
 impl Scanner {
     pub fn new(base_url: Url) -> Self {
         Self {
@@ -180,11 +32,28 @@ impl Scanner {
     }
 
     pub async fn capabilities(&self) -> Result<ScannerCapabilities, Error> {
+        self.send_get_request(self.extended_url(&["eSCL", "ScannerCapabilities"]))
+            .await
+    }
+
+    pub async fn status(&self) -> Result<ScannerStatus, Error> {
+        self.send_get_request(self.extended_url(&["eSCL", "ScannerStatus"]))
+            .await
+    }
+
+    fn extended_url(&self, segments: &[&'static str]) -> Url {
         let mut url = self.base_url.clone();
         url.path_segments_mut()
             .expect("Invalid base URL")
-            .extend(&["eSCL", "ScannerCapabilities"]);
+            .extend(segments);
 
+        url
+    }
+
+    async fn send_get_request<T>(&self, url: Url) -> Result<T, Error>
+    where
+        T: DeserializeOwned,
+    {
         let response = self
             .http_client
             .get(url)
---
+++ src/status.rs
@@ -0,0 +1,65 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct ScannerStatus {
+    pub version: String,
+    pub state: ScannerState,
+    pub jobs: Jobs,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub enum ScannerState {
+    /// Idle
+    Idle,
+    /// Busy with some job or activity
+    Processing,
+    /// Calibrating, preparing the unit
+    Testing,
+    /// Error condition occurred
+    Stopped,
+    /// Unit is unavailable
+    Down,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct Jobs {
+    pub job_info: Vec<JobInfo>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct JobInfo {
+    pub job_uri: String,
+    pub job_uuid: String,
+    pub age: u32,
+    pub images_completed: u32,
+    pub images_to_transfer: u32,
+    pub job_state: JobState,
+    pub job_state_reasons: JobStateReasons,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub enum JobState {
+    /// End state – indicates that the job was canceled either by the remote client application
+    /// (thru the eSCL interface) or by the user interacting with the scanner directly. Check
+    /// [JobStateReasons] for more details.
+    Canceled,
+    /// End state – either an internal device error, or a communication error or a security error
+    Aborted,
+    /// Job is finished successfully
+    Completed,
+    /// The job was initiated, and the scanner is preparing the scan engine
+    Pending,
+    /// The scanner is processing the job and is transmitting the scan data
+    Processing,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct JobStateReasons {
+    pub job_state_reason: String,
+}
